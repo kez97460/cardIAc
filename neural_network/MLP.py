5578ab6b39d11c55a38a39c6683483313c992aef
Lïam Lotte <sergentzeug@gmail.com>
2024-09-18 15:32:53 +0200

    empty commit message

--- neural_network/MLP.py
+++ neural_network/MLP.py
@@ -1,6 +1,3 @@
-import os
-
-
 # TensorFlow and tf.keras
 import tensorflow as tf
 
@@ -11,8 +8,3 @@ import matplotlib.pyplot as plt
 train_dataset = np.load("./neural_network/dataset_publications/MLP/data/data/X_test.npy", 'r')
 print(train_dataset.shape)
 
-plt.figure()
-plt.imshow(train_dataset[0])
-plt.colorbar()
-plt.grid(False)
-plt.show()

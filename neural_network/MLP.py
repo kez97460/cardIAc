1eadd3f5e55e8e02da837748ce62e605d447a00a
Lïam Lotte <sergentzeug@gmail.com>
2024-09-27 22:14:49 +0200

    some try on database

--- neural_network/MLP.py
+++ neural_network/MLP.py
@@ -5,6 +5,29 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
-train_dataset = np.load("./neural_network/dataset_publications/MLP/data/data/X_test.npy", 'r')
-print(train_dataset.shape)
 
+train_dataset = np.load("./neural_network/dataset_publications/MLP/data/data/X.npy")
+print("Training Dataset Shape :", train_dataset.shape)
+train_dataset = train_dataset[::,:-2]
+
+result_dataset = np.load("./neural_network/dataset_publications/MLP/data/data/y.npy")
+print("Test Dataset Shape :", result_dataset.shape)
+
+# Print the whole dataset in one graph to see the global shape
+for i in range(5000) :
+    plt.plot(np.arange(0, (train_dataset.shape[1])), train_dataset[i])
+    plt.subplot()
+
+plt.show()
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(181)),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(128)
+])
+
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+model.fit(train_dataset, result_dataset, epochs=20)
